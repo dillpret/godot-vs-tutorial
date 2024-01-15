dc27129cc66cbe881eb425c0938b3341318ddcb0
Dillon Pretorius <pretorius.dillon@gmail.com>
2024-01-15 21:48:56 +0200

    finished tutorial

---
+++ 2d-project-start/survivors_game.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+func _ready():
+	spawn_mob()
+	spawn_mob()
+	spawn_mob()
+	spawn_mob()
+	spawn_mob()
+
+func spawn_mob():
+	const MOB = preload("res://characters/mob.tscn")
+	var new_mob = MOB.instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	new_mob.global_position = %PathFollow2D.global_position
+	add_child(new_mob)
+
+
+func _on_timer_timeout():
+	spawn_mob()
+
+
+func _on_player_health_depleted():
+	%GameOver.visible = true
+	get_tree().paused = true
